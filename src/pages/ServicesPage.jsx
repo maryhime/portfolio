ccbb32508f1470fc2cec2645c25d487e434e6eba
Mary Therese Caseria <caseria.therese@gmail.com>
2023-06-30 11:29:13 +0800

    #6 added services block

---
+++ src/pages/ServicesPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeadingsComponent from '../components/generic/HeadingsComponent'
+
+const ServicesPage = () => {
+  return (
+    <div className="">
+      <HeadingsComponent subheading={'what i will do for you'} heading={'services i offer'} />
+
+    </div>
+  )
+}
+
+export default ServicesPage
