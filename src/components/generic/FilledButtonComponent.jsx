7dbd9fe92e4f9a3c9681072d19c2dbe73bb36771
Mary Therese Caseria <caseria.therese@gmail.com>
2023-07-04 16:16:07 +0800

    #9 added floating window

--- src/components/generic/FilledButtonComponent.jsx
+++ src/components/generic/FilledButtonComponent.jsx
@@ -1,19 +1,20 @@
 import React, { Fragment } from 'react'
 import { ICONS } from '../../utils/icons'
 
-const FilledButtonComponent = ({text, hidden=true}) => {
+const FilledButtonComponent = ({text, hidden=true ,width}) => {
   return (
-    <div className="flex flex-row bg-accent-1 px-20 rounded-button laptop:h-button phone:h-button-m items-center text-text-color uppercase w-[156px] justify-center gap-8 phone:text-btn-m laptop:text-btn-lg hover:bg-accent-2">
+    <div className={`flex flex-row bg-accent-1 px-20 rounded-button laptop:h-button phone:h-button-m items-center text-text-color uppercase  justify-center gap-8 phone:text-btn-m laptop:text-btn-lg hover:bg-accent-2 ${width}`}>
       {
       hidden ?  <div>
       {text}
     </div> : (<Fragment>
-      <div>
-        {text}
-      </div>
+      
       {ICONS.map((value, index) => (
         <img key={index} src={`${value.message}`} className='phone:w-[20px] phone:h-[20px]' alt="" />
       ))}
+      <div>
+        {text}
+      </div>
     </Fragment>)
      }
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React, { Fragment } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { PAGES } from './routes'
+import FloatingWindowComponent from './components/FloatingWindowComponent'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <FloatingWindowComponent/>
     <div className="bg-[url('/images/background.svg')] ] bg-no-repeat bg-cover h-screen w-screen overflow-x-hidden">
       {PAGES.map((value, index) =>
         <Fragment key={index} >
