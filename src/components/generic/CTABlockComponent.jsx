d5cc70a184a9ed1461125adbb98ae5fb8af7eb0d
Mary Therese Caseria <caseria.therese@gmail.com>
2023-07-06 10:02:19 +0800

    #12 updated CTA Block mobile gap and background

--- src/components/generic/CTABlockComponent.jsx
+++ src/components/generic/CTABlockComponent.jsx
@@ -4,7 +4,7 @@ import FilledButtonComponent from './FilledButtonComponent'
 
 const CTABlockComponent = () => {
   return (
-    <div className="bg-[url('/images/CTA-bg.svg')] ] bg-no-repeat bg-cover flex flex-col tablet:flex-row font-inter py-100  px-24 phone-lg:px-64 tablet:px-50 laptop-lg:px-100 desktop:px-200  w-full items-center gap-100">
+    <div className="bg-[url('/images/CTA-mobile.svg')] tablet:bg-[url('/images/CTA-bg.svg')] bg-no-repeat bg-cover flex flex-col tablet:flex-row font-inter py-100  px-24 phone-lg:px-64 tablet:px-50 laptop-lg:px-100 desktop:px-200 w-full items-center gap-24 tablet:gap-100">
       <div className="flex flex-col gap-24 laptop:gap-24 text-text-color">
         <h3 className="text-phone-h3 tablet:text-tablet-h3 laptop:text-h3 uppercase tablet:text-left text-center ">
           Have any Project in Mind?
